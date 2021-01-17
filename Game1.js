3f12dfd8aa00744489e0eb240b2ed1a6902e8922
Azm <Azm@mail.ru>
2021-01-17 19:51:13 +0500

    toOOP

--- Game1.js
+++ Game1.js
@@ -1,99 +1,121 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+var cvs;
+var ctx;
+var xPosBeg;
+var yPosBeg;
+var bodyTank;
+var tower;
+var shell;
 
-var body = new Image(47, 90);  //body of tank
-var tower = new Image(40, 72);  //tower of tank
-var shell = new Image();
-shell.width = 12;
-shell.height = 12;
-body.src = "img/body.png";
-tower.src = "img/tower.png";
-shell.src = "img/shell.png"; 
+window.onload = startGame;
 
-var xPos = cvs.width / 2 - body.width / 2, //position of tank
-    yPos = cvs.height - body.height - 10;
-
-var xPos_shell = 0, yPos_shell = 0;  //смещение снаряда
-
-var ang_tow = 0; //angle rotation tower 
-var ang_shell = 0; //angle shell 
-var dx, dy; //set the center of rotation tower
-
-var counter = 0; //global counter for reloading of shell 
+var xSh = 0, ySh = 0;  //position of shell
 
 document.addEventListener("keydown", function(e) { keyDown(e);});
-cvs.onmousemove = mouseMov;
-cvs.onmousedown = mouseDown;
+
+class Component{
+    constructor (xPos, yPos,srcImage, type){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.angRotate = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.image = new Image(); 
+        this.image.src = srcImage;
+        this.type = type; //shell, tower, body
+    }
+    
+    update (xPosShell = 0, yPosShell = 0){
+        this.dx = this.xPos + 23;
+        this.dy = this.yPos + 45;
+        //if (this.type != 'body'){
+            ctx.save();
+            ctx.translate(this.dx, this.dy);
+            ctx.rotate(this.angRotate);
+            ctx.translate(-this.dx, -this.dy)
+            ctx.drawImage(this.image, this.xPos + xPosShell, this.yPos + yPosShell);
+            ctx.restore();
+        
+        //else ctx.drawImage(this.image, this.xPos, this.yPos);
+    }
+
+    calcAngle (posMouse){
+        
+        let a = 1, b = 1; //for angle of shell
+
+        if (this.type == 'shell') {a = 1.5; b = 0.5;}
+        let deltaY = this.dy  - posMouse.clientY,  //delta is difference coordinates mouse event
+            deltaX = posMouse.clientX - this.dx;   //and tank coordin.
+        this.angRotate = Math.atan(deltaY / deltaX);
+        if (deltaX < 0)
+            this.angRotate = - a * Math.PI / 2 - this.angRotate;  //part 1 and 4 in Trigonom. Circle
+        else 
+            this.angRotate =  b * Math.PI / 2 - this.angRotate; //part 2 and 3
+    }
+    // xPos_shell = 0; yPos_shell = 0;
+
+}
+
+function startGame(){
+    cvs = document.getElementById("canvas");
+    ctx = cvs.getContext("2d");
+
+    cvs.onmousemove = (pos) => {
+        tower.calcAngle(pos);
+    };
+
+    cvs.onmousedown = (pos2) => {
+        xSh = 0; ySh = 0;
+        shell.calcAngle(pos2);
+    };
+
+    xPosBeg = cvs.width / 2 - 23;  //Begining position x of tank
+    yPosBeg = cvs.height - 100;    //Begining position y of tank
+
+    bodyTank = new Component (xPosBeg, yPosBeg, "img/body.png", 'body');
+    tower = new Component (xPosBeg, yPosBeg, "img/tower.png", 'tower');
+    shell = new Component (xPosBeg, yPosBeg, "img/shell.png", 'shell');
+
+    draw();
+}
 
 function draw(){
+
     ctx.clearRect(0, 0 , cvs.width, cvs.height); //clearing of canvas
+
+    bodyTank.update();
+    xSh += 6;
+    ySh -= 6;
+
+    shell.update(xSh + 17, ySh + 36);
+    tower.update(6, -7);  //correcting center of tower
     
-    dx = xPos + body.width / 2;  
-    dy = yPos + body.height / 2;
-
-    ctx.drawImage(body, xPos, yPos);
-   
-    /*угол наклона снаряда*/
-    if (counter != 0 && counter < 150) {
-        xPos_shell += 6;
-        yPos_shell -= 6;
-        ctx.save();
-        ctx.translate(dx, dy);
-        ctx.rotate(ang_shell);
-        ctx.translate(-dx, -dy)
-        ctx.drawImage(shell , xPos + tower.width / 2 + xPos_shell, yPos + tower.height / 2 + yPos_shell);
-        ctx.restore();
-        counter++;
-    }
-    else counter = 0;
-    //console.log (counter);
-
-    /*Определение положения башни*/
-    ctx.save();
-    ctx.translate(dx, dy);
-    ctx.rotate(ang_tow);
-    ctx.translate(-dx, -dy)
-    ctx.drawImage(tower, xPos + 6, yPos - 7);
-    ctx.restore();
-    /*********конец опред башни***/
-
     requestAnimationFrame(draw);
 }
 
 function keyDown(e){
     switch (e.keyCode){
-        case 87: // W
-            yPos -= 2;
+        case 65:
+            //bodyTank.angRotate -= Math.PI / 180;
+            // console.log(bodyTank.angRotate);
+            break;
+        case 68:
+            //bodyTank.angRotate += Math.PI / 180; 
+            break;
+        case 87: // 
+            bodyTank.yPos -= 2;
+            tower.yPos -= 2;
+            shell.yPos -= 2;
             break;
         case 83: // S - back
-            yPos += 2;
+            bodyTank.yPos += 2;
+            tower.yPos += 2;
+            shell.yPos += 2;
             break;
 
     }
 }
-function mouseMov(pos){
-    var deltaY = dy  - pos.clientY,  //delta is difference coordinates mouse event
-        deltaX = pos.clientX - dx;   //and tank coordin.
-    ang_tow = Math.atan(deltaY / deltaX);
-    if (deltaX < 0)
-        ang_tow = - Math.PI / 2 - ang_tow;  //part 1 and 4 in Trigonom. Circle
-    else 
-        ang_tow =  Math.PI / 2 - ang_tow; //part 2 and 3*/
-}
 
-function mouseDown(pos){
-    if (counter == 0){
-        var deltaY = dy  - pos.clientY,  //delta is difference coordinates mouse event
-            deltaX = pos.clientX - dx;   //and tank coordin.
-        ang_shell = Math.atan(deltaY / deltaX);
-        if (deltaX < 0)
-            ang_shell = - 3 * Math.PI / 4 - ang_shell;  //part 1 and 4 in Trigonom. Circle
-        else 
-            ang_shell =  Math.PI / 4 - ang_shell; //part 2 and 3
-        xPos_shell = 0; yPos_shell = 0;
-        counter++;
-    }
 
-}
 
-tower.onload = draw;
+
+
